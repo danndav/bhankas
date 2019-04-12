0d0dd252be13d8eb0983d064e557a047d2c62c33
dandave <danielimodoye@gmail.com>
2019-04-12 18:15:41 +0100

    ch(server):write an end point for each route

--- server/app.js
+++ server/app.js
@@ -1,21 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
-import apiVersion1 from './versioning/v1'
+import apiVersion1 from './versioning/v1';
+
 
 const app = express();
 
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true,
 }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use('/api/v1', apiVersion1);
 
 app.get('/', (req, res) => res.send({
-    ok: true,
-    message: 'Welcome to Banka',
-    baseurl: '/api/{version}'
+  ok: true,
+  message: 'Welcome to Banka',
+  baseurl: '/api/{version}',
 }).status(200));
 
 // No routes matched? 404.
@@ -24,11 +25,11 @@ app.use((req, res) => res.status(404).send('Sorry that route/method doesnt exist
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    if (process.env.NODE_ENV === 'dev') {
-        console.log(`The Dev server is running on port ${PORT}`);
-    } else {
-        console.log(`The production server is now running at ${PORT}`);
-    }
+  if (process.env.NODE_ENV === 'test') {
+    console.log(`The Dev server is running on port ${PORT}`);
+  } else {
+    console.log(`The production server is now running at ${PORT}`);
+  }
 });
 
 export default app;
